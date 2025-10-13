8b5f1ef7e6fdb0e3a021b7be9647363c07475458
Vinicius <viniciusdelucaprado@gmail.com>
2025-10-13 14:14:00 -0300

    calendario33

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/layout/Header';
 import Calendar from './components/events/Calendar';
 import { buscarFestas } from './lib/api';
